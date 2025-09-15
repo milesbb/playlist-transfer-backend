d810462b6ec0fad013914a337031c527d5709b71
Miles Bailey-Braendgaard <milesbb101@gmail.com>
2025-09-15 16:15:15 +0100

    fixed migration

--- migrations/V2__add_refresh_token_table.sql
+++ migrations/V2__add_refresh_token_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE refresh_tokens (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   token_hash TEXT NOT NULL,
   expires_at TIMESTAMPTZ NOT NULL,
   revoked_at TIMESTAMPTZ,
