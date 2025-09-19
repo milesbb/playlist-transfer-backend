a6169466f2e5a7665f3cb11887c809e76935ab71
Miles Bailey-Braendgaard <milesbb101@gmail.com>
2025-09-19 10:55:37 +0100

    added CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
 import express from 'express';
 import routers from '@controllers/index';
+import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://github.com/milesbb/playlist-transfer-frontend',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS not allowed for this origin'));
+      }
+    },
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
